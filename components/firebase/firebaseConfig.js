1af2ab8c5b0c5df05448b24b98bf5f8b390f99a7
solaris7x <question211199@gmail.com>
2020-09-24 22:27:51 +0530

    Updated Readme
    	new file:   .env-sample
    	deleted:    .vscode/settings.json
    	modified:   README.md
    	modified:   components/firebase/firebaseConfig.js
    	modified:   package.json

--- components/firebase/firebaseConfig.js
+++ components/firebase/firebaseConfig.js
@@ -3,15 +3,26 @@ import "firebase/firestore";
 import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD7Ez5Og9MvqBGov_-wZdEwFPyEVbK5B_E",
-  authDomain: "quoteswatch.firebaseapp.com",
-  databaseURL: "https://quoteswatch.firebaseio.com",
-  projectId: "quoteswatch",
-  storageBucket: "quoteswatch.appspot.com",
-  messagingSenderId: "294695320608",
-  appId: "1:294695320608:web:7413cf244b546667871045",
-  measurementId: "G-QNJ62NZDQN",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_DATABASEURL,
+  projectId: process.env.NEXT_PUBLIC_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_APPID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENTID,
 };
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyD7Ez5Og9MvqBGov_-wZdEwFPyEVbK5B_E",
+//   authDomain: "quoteswatch.firebaseapp.com",
+//   databaseURL: "https://quoteswatch.firebaseio.com",
+//   projectId: "quoteswatch",
+//   storageBucket: "quoteswatch.appspot.com",
+//   messagingSenderId: "294695320608",
+//   appId: "1:294695320608:web:7413cf244b546667871045",
+//   measurementId: "G-QNJ62NZDQN",
+// };
 
 const samplefirebaseConfig = {
   apiKey: "API_KEY",
